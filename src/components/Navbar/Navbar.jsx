88b53aff68028cefe47aa000f5606261415dee6a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-19 00:44:02 +0600

    update navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 
-
+import img from "../../assets/images/logo.png"
 const Navbar = () => {
     return (
-        <div>
-            <h3>This is a navbar section </h3>
+        <div className="flex items-center">
+            <img src={img} alt="" />
+            <div className="flex justify-around text-white w-full ">
+                <a href="/residencies">Residencies</a>
+                <a href="/value">Our Value</a>
+                <a href="/contact">Contact Us</a>
+                <a href="/started">Get Started</a>
+                <button className="text-white px-8 py-2  rounded-lg  bg-blue-500">Learn More</button>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Main from './components/Main/Main';
+import Home from './components/Home/Home';
 const router = createBrowserRouter([
   {
     path: "/",
     element:<Main></Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/Residencies",
+        element:<Home></Home>
+      },
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(

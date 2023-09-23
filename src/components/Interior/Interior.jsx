271c48ed027be5fec3f895f05ca1207d93c77582
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-24 01:46:48 +0600

    update interior design

--- src/components/Interior/Interior.jsx
+++ src/components/Interior/Interior.jsx
@@ -9,13 +9,16 @@ import img7 from "../../assets/interior design/rome8.avif"
 import img8 from "../../assets/interior design/rome9.avif"
 import img9 from "../../assets/interior design/rome10.avif"
 import img10 from "../../assets/interior design/rome11.avif"
+import img11 from "../../assets/interior design/rome12.avif"
 
 const Interior = () => {
     return (
-        <div className="grid grid-cols-2 m-20 p-20">
+        <div>
+            <h3 className="text-3xl font-bold p-10 text-center">Our <span className="text-yellow-400">Interior</span> Design</h3>
+            <div className="grid grid-cols-2 lg:mx-20 lg:px-20">
             <img className="p-2" src={img} alt="" />
             <img className="p-2"  src={img1} alt="" />
-            <img className="p-2"  src={img2} alt="" />
+            <img className="p-2 object-cover"  src={img2} alt="" />
             <img  className="p-2" src={img3} alt="" />
             <img className="p-2"  src={img4} alt="" />
             <img className="p-2"  src={img5} alt="" />
@@ -24,6 +27,8 @@ const Interior = () => {
             <img className="p-2"  src={img8} alt="" />
             <img className="p-2"  src={img9} alt="" />
             <img className="p-2"  src={img10} alt="" />
+            <img className="p-2"  src={img11} alt="" />
+        </div>
         </div>
     );
 };

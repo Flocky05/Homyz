50338945ba9c80cef6b4b1f25d3c68664c10ab93
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-30 21:49:13 +0600

    added people says card section

--- src/components/PeopleSays/PeopleSays.jsx
+++ src/components/PeopleSays/PeopleSays.jsx
@@ -15,12 +15,34 @@ const PeopleSays = () => {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, rem iure voluptatum est ab suscipit pariatur excepturi nostrum omnis ex, dolorem aspernatur! Perspiciatis ratione saepe porro. Accusantium facilis velit consequuntur.</p>
                     </div>
                 </div>
-                <div>
-                    <div className="hover:bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4  w-1/4 rounded-lg">
-                        <h1 className="text-4xl font-bold">``</h1>
+                <div className="grid grid-cols-3 m-4 overflow-hidden p-4">
+                    <div className="hover:bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4  w-full rounded-lg">
+                        <h1 className="text-4xl font-bold text-blue-500">``</h1>
                         <p>Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.</p>
                         <div className="flex gap-4 justify-items-center p-4">
                             <img className="w-10 h-10 rounded-full" src={img} alt="" />
+                            <div>
+                                <h3 className="text-lg ">Rahul</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hover:bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4 rounded-lg w-full">
+                        <h1 className="text-4xl font-bold text-blue-500">``</h1>
+                        <p>Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.</p>
+                        <div className="flex gap-4 justify-items-center p-4">
+                            <img className="w-10 h-10 rounded-full" src={img1} alt="" />
+                            <div>
+                                <h3 className="text-lg ">Rahul</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hover:bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4  w-full rounded-lg">
+                        <h1 className="text-4xl font-bold text-blue-500">``</h1>
+                        <p>Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.</p>
+                        <div className="flex gap-4 justify-items-center p-4">
+                            <img className="w-10 h-10 rounded-full" src={img2} alt="" />
                             <div>
                                 <h3 className="text-lg ">Rahul</h3>
                                 <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>

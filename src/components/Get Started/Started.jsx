714a4eb1b6ff3d6470efa0059b6f73763bb4c2b5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-30 09:18:54 +0600

    update getStart section

--- src/components/Get Started/Started.jsx
+++ src/components/Get Started/Started.jsx
@@ -11,28 +11,31 @@ const Started = () => {
                     <h2 className="py-4">Find a better card deal </h2>
                     <h2 className="py-4">in few easy steps.</h2>
                 </div>
-                <p>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
-                <button className="bg-gradient-to-r from-green-300 to-blue-300 px-6 py-2 rounded-lg text-black mt-4">Get Start</button>
+                <p className="py-4">Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+                <button className="bg-gradient-to-r from-gray-700 to-gray-300 px-6 py-2 rounded-lg text-white font-semibold mt-6">Get Start</button>
             </div>
-            <div className="relative ml-8">
-                <div className="p-8 border-4 border-green-500 rounded-full absolute ml-52 -mt-32"> </div>
-                <div className="p-20 border-4 border-green-500 rounded-full absolute ml-40 -mt-44"></div>
-                <div className="p-32 border-8 border-green-500 rounded-full absolute ml-28 -mt-56"></div>
+            <div className="grid items-center place-content-center content-center relative ml-8">
+                <div className="border-4 border-gray-500 rounded-full absolute h-32 w-32 left-32"> </div>
+                <div className="border-4 border-gray-500 rounded-full absolute h-64 w-64 left-16"></div>
+                <div className="border-8 border-gray-500 rounded-full absolute h-96 w-96 left-0"></div>
+                {/* card1 start */}
                 <div className="bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4 absolute w-1/4 rounded-lg -mt-48">
                         <h3 className="text-md text-center">Scan your Id Card</h3>
                         <p className="text-sm text-center">Lorem ipsum dolor sit, amet  Quisquam.</p>
                 </div>
+                {/* Card2 start  */}
                 <div className="bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4 absolute w-2/5 rounded-lg -mt-64 ml-60">
                         <h3 className="text-md text-center">Scan your Id Card</h3>
                         <p className="text-sm text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam corrupti vero culpa harum  voluptates corporis tenetur fugiat!</p>
                 </div>
-                <div className="bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4 absolute w-2/3 rounded-lg -mt-12 ml-48">
+                {/* Card3 start */}
+                <div className="bg-gradient-to-r from-gray-600 to to-black text-white m-8 p-4 absolute w-2/3 rounded-lg mt-64 ml-36">
                         <h3 className="text-md text-center">Payment Method</h3>
-                        <div className="grid grid-cols-4 p-4">
-                            <img className="w-10" src={img1} alt="" />
-                            <img className="w-10" src={img2} alt="" />
-                            <img className="w-10" src={img3} alt="" />
-                            <img className="w-10" src={img4} alt="" />
+                        <div className="grid grid-cols-4 p-4 justify-items-center">
+                            <img  className="w-10 bg-white rounded-lg p-2" src={img1} alt="" />
+                            <img className="w-10 bg-white rounded-lg p-2" src={img2} alt="" />
+                            <img className="w-10 bg-white rounded-lg p-2" src={img3} alt="" />
+                            <img className="w-10 bg-white rounded-lg p-2" src={img4} alt="" />
                         </div>
                 </div>
             </div>
